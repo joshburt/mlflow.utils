066eb52c93cecaa0f36a9e29fbaf08550b233c4f
Joshua Burt <joshburt@Joshuas-MBP.lan>
2022-12-09 13:03:33 -0800

    more output and handling

--- src/anaconda/ae5/secrets.py
+++ src/anaconda/ae5/secrets.py
@@ -4,9 +4,12 @@ from pathlib import Path
 
 def load_ae5_user_secrets(secrets_path: str = "/var/run/secrets/user_credentials/"):
     base_path: Path = Path(secrets_path)
-    print(f"Loading environment variables from {base_path}:")
-    for secret in base_path.glob("*"):
-        if secret.is_file():
-            print(secret.name)
-            with open(file=secret, mode="r", encoding="utf-8") as file:
-                os.environ[secret.name] = file.read()
+    if base_path.exists():
+        print(f"Loading environment variables from {base_path}:")
+        for secret in base_path.glob("*"):
+            if secret.is_file():
+                print(secret.name)
+                with open(file=secret, mode="r", encoding="utf-8") as file:
+                    os.environ[secret.name] = file.read()
+    else:
+        print(f"Skipping loading AE5 user secrets, specified secrets path not found: {secrets_path}")
