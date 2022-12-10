03322aec58b5de9f6265f47ebef38f003e113a75
Joshua Burt <joshburt@Joshuas-MBP.lan>
2022-12-09 16:25:34 -0800

    serving

--- src/anaconda/mlflow/tracking/utils.py
+++ src/anaconda/mlflow/tracking/utils.py
@@ -9,3 +9,8 @@ def load_model(model_name: str, model_version: int) -> PyFuncModel:
 
 def load_model_by_run(logged_model: str) -> PyFuncModel:
     return mlflow.pyfunc.load_model(model_uri=logged_model)
+
+
+def load_model_by_stage(model_name: str, stage: str) -> PyFuncModel:
+    model_uri: str = f"models:/{model_name}/{stage}"
+    return load_model_by_run(logged_model=model_uri)
